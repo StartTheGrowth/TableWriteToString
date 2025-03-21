57057f0a897968b248f162ce5c3f0cdcc0d5fa58
Vladimir <vladimir118311@gmail.com>
2025-03-21 22:38:52 +0300

    Change pom.xml

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,7 +1,6 @@
 import java.io.IOException;
 import java.util.Scanner;
-import org.apache.poi.ss.usermodel.Workbook;
-import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+
 
 public class Main {
     public static void main(String[] args) throws IOException {
